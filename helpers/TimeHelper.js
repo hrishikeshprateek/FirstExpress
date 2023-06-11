6351df86b06e8e1ba832286bb1d56248ff25ad56
hrishikeshprateek <prateekhrishikesh@gmail.com>
2023-06-11 16:13:38 +0530

    1.0 commit for publicApis.js

--- helpers/TimeHelper.js
+++ helpers/TimeHelper.js
@@ -5,6 +5,19 @@ function formatTime(seconds) {
 
     return `${hours} hours, ${minutes} minutes, ${remainingSeconds} seconds`;
 }
+
+function convertDateStringToDDMMYYYY(dateString){
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
+
+    return `${day}/${month}/${year}`;
+}
+
+const rani = 'girl'
 module.exports = {
-    formatTime
+    formatTime,
+    convertDateStringToDDMMYYYY,
+    rani
 }
--- routes/public/publicApis.js
+++ routes/public/publicApis.js
@@ -105,6 +105,77 @@ function processLog(res,arrangeBy,logFilePath) {
 
 }
 
+function processLogPhoneSpecific(res, phoneNo, logFilePath) {
+    console.log('started to analyze the log for the number'+ phoneNo+' ....')
+
+    // Read the log file
+    fs.readFile(logFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading log file:', err);
+        }
+
+        // Parse the JSON data
+        let logEntries;
+        try {
+            logEntries = JSON.parse(data);
+            //console.log(logEntries)
+        } catch (err) {
+            console.error('Error parsing log file:', err);
+        }
+
+        // Process the log data
+        const contactDetails = [];
+        let name = '';
+        let totalTalkTime = 0;
+        let OutgoingCount = 0;
+        let IncomingCount = 0;
+        let MissedCallCount = 0;
+
+
+        for (const entry of logEntries) {
+            const { NUMBER ,CALL_DATE, CALL_TYPE, DURATION, NAME } = entry;
+            if (NUMBER && DURATION && NUMBER.toString().replace(/(\+91|91)/g,'').trim() ===
+                phoneNo.replace(/(\+91|91)/g,'').trim() ) {
+                name = NAME;
+                totalTalkTime += parseInt(DURATION)
+                if (CALL_TYPE){
+                    if (CALL_TYPE.toString().toLowerCase() ==='incoming') IncomingCount++;
+                    else if (CALL_TYPE.toString().toLowerCase() === 'outgoing') OutgoingCount ++;
+                    else if (CALL_TYPE.toString().toLowerCase() === 'missed')MissedCallCount ++;
+                }
+                const existingContact = contactDetails.find((contact) => contact.CALL_DATE === time.convertDateStringToDDMMYYYY(CALL_DATE));
+
+                if (existingContact){
+                    existingContact.CALL_COUNT ++
+                    existingContact.DURATION += parseInt(DURATION)
+                }else contactDetails.push({
+                    CALL_DATE : time.convertDateStringToDDMMYYYY(CALL_DATE),
+                    CALL_COUNT : 1,
+                    DURATION : parseInt(DURATION)
+                })
+            }
+        }
+
+        contactDetails.forEach((contact) => {
+            contact.DURATION = time.formatTime(contact.DURATION);
+        });
+
+        // Send the response
+        res.json([{
+            QueryNumber : phoneNo,
+            NameSaved : name,
+            TotalTalkTime : time.formatTime(totalTalkTime),
+            OutgoingCount : OutgoingCount,
+            IncomingCount : IncomingCount,
+            MissedCount : MissedCallCount,
+            TotalRecordDays : contactDetails.length
+        },
+            contactDetails,
+        ]);
+
+    });
+}
+
 router.post('/logs/analyze', upload.single('logFile'), (req, res) => {
     // Access the uploaded file using req.file
     const file = req.file
@@ -112,6 +183,16 @@ router.post('/logs/analyze', upload.single('logFile'), (req, res) => {
     if (file){
         if (!file.originalname.toString().endsWith('.json')) res.json({error : 'not a valid supported log file type, try uploading a valid .json log file.'})
         else processLog(res, req.query.arrangeBy, file.path)
+    }else res.json({error : 'failed to get a valid log file'})
+});
+
+router.post('/logs/analyze/queryPhone', upload.single('logFile'), (req, res) => {
+    // Access the uploaded file using req.file
+    const file = req.file
+    console.log(req.file)
+    if (file){
+        if (!file.originalname.toString().endsWith('.json')) res.json({error : 'not a valid supported log file type, try uploading a valid .json log file.'})
+        else processLogPhoneSpecific(res, req.query.phoneNo, file.path)
     }else res.json({error : 'failed to get a valid log file'})
 });
 
