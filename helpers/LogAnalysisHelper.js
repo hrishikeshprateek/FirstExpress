6b66410b928eefce05413f97efc4cc5340b9f8df
hrishikeshprateek <prateekhrishikesh@gmail.com>
2023-06-13 18:51:22 +0530

    1.0 commit for publicApis.js

---
+++ helpers/LogAnalysisHelper.js
@@ -0,0 +1,158 @@
+const fs = require('fs');
+const time = require('../helpers/TimeHelper')
+
+function processLog(res,arrangeBy,logFilePath) {
+    console.log('started...')
+
+    // Read the log file
+    fs.readFile(logFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading log file:', err);
+        }
+
+        // Parse the JSON data
+        let logEntries;
+        try {
+            logEntries = JSON.parse(data);
+            //console.log(logEntries)
+        } catch (err) {
+            console.error('Error parsing log file:', err);
+        }
+
+        // Process the log data
+        const contactDetails = [];
+
+        for (const entry of logEntries) {
+            const { NUMBER , DURATION, NAME } = entry;
+
+            if (NUMBER && DURATION) {
+                const existingContact = contactDetails.find((contact) => contact.NUMBER === NUMBER);
+
+                if (existingContact) {
+                    existingContact.callCount++;
+                    existingContact.DURATION += parseInt(DURATION);
+                } else {
+                    contactDetails.push({
+                        NUMBER,
+                        callCount: 1,
+                        DURATION : parseInt(DURATION),
+                        NAME : NAME
+                    });
+                }
+            }
+        }
+
+        if (arrangeBy) {
+            if (arrangeBy.toString().toLowerCase() === 'duration')
+                contactDetails.sort((a, b) => b.DURATION - a.DURATION);
+            else if (arrangeBy.toString().toLowerCase() === 'count')
+                contactDetails.sort((a, b) => b.callCount - a.callCount);
+        }
+
+
+        // Find the most contacted number and calculate total talk time
+        let mostContactedNumber = null;
+        let maxCount = 0;
+        let totalTalkTime = 0;
+
+        for (const contact of contactDetails) {
+            const { NUMBER, callCount, DURATION } = contact;
+
+            if (callCount > maxCount) {
+                mostContactedNumber = NUMBER;
+                maxCount = callCount;
+            }
+
+            totalTalkTime += parseInt(DURATION);
+        }
+
+        contactDetails.forEach((contact) => {
+            contact.DURATION = time.formatTime(contact.DURATION);
+        });
+
+        // Send the response
+        res.json({
+            mostContactedNumber,
+            contactDetails,
+            totalTalkTime : time.formatTime(totalTalkTime)
+        });
+
+    });
+
+}
+
+function processLogPhoneSpecific(res, phoneNo, logFilePath) {
+    console.log('started to analyze the log for the number'+ phoneNo+' ....')
+
+    // Read the log file
+    fs.readFile(logFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading log file:', err);
+        }
+
+        // Parse the JSON data
+        let logEntries;
+        try {
+            logEntries = JSON.parse(data);
+            //console.log(logEntries)
+        } catch (err) {
+            console.error('Error parsing log file:', err);
+        }
+
+        // Process the log data
+        const contactDetails = [];
+        let name = '';
+        let totalTalkTime = 0;
+        let OutgoingCount = 0;
+        let IncomingCount = 0;
+        let MissedCallCount = 0;
+
+
+        for (const entry of logEntries) {
+            const { NUMBER ,CALL_DATE, CALL_TYPE, DURATION, NAME } = entry;
+            if (NUMBER && DURATION && NUMBER.toString().replace(/(\+91|91)/g,'').trim() ===
+                phoneNo.replace(/(\+91|91)/g,'').trim() ) {
+                name = NAME;
+                totalTalkTime += parseInt(DURATION)
+                if (CALL_TYPE){
+                    if (CALL_TYPE.toString().toLowerCase() ==='incoming') IncomingCount++;
+                    else if (CALL_TYPE.toString().toLowerCase() === 'outgoing') OutgoingCount ++;
+                    else if (CALL_TYPE.toString().toLowerCase() === 'missed')MissedCallCount ++;
+                }
+                const existingContact = contactDetails.find((contact) => contact.CALL_DATE === time.convertDateStringToDDMMYYYY(CALL_DATE));
+
+                if (existingContact){
+                    existingContact.CALL_COUNT ++
+                    existingContact.DURATION += parseInt(DURATION)
+                }else contactDetails.push({
+                    CALL_DATE : time.convertDateStringToDDMMYYYY(CALL_DATE),
+                    CALL_COUNT : 1,
+                    DURATION : parseInt(DURATION)
+                })
+            }
+        }
+
+        contactDetails.forEach((contact) => {
+            contact.DURATION = time.formatTime(contact.DURATION);
+        });
+
+        // Send the response
+        res.json([{
+            QueryNumber : phoneNo,
+            NameSaved : name,
+            TotalTalkTime : time.formatTime(totalTalkTime),
+            OutgoingCount : OutgoingCount,
+            IncomingCount : IncomingCount,
+            MissedCount : MissedCallCount,
+            TotalRecordDays : contactDetails.length
+        },
+            contactDetails,
+        ]);
+
+    });
+}
+
+module.exports = {
+    processLog,
+    processLogPhoneSpecific
+}
--- routes/public/publicApis.js
+++ routes/public/publicApis.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const multer = require('multer');
-const Users = require("../../schema/user");
-const fs = require('fs');
-const time = require('../../helpers/TimeHelper')
-
+const logAnalyser = require('../../helpers/LogAnalysisHelper')
 const publicDataHelper = require('../../helpers/PublicDataHelper')
+
 const router = express.Router();
 
 const upload = multer({ dest: 'uploads/' });
@@ -25,164 +23,13 @@ router.get('/menu/dealOfTheDay', (req, res) =>{
     publicDataHelper.getDealOfTheDay(res)
 })
 
-function processLog(res,arrangeBy,logFilePath) {
-    console.log('started...')
-
-    // Read the log file
-    fs.readFile(logFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading log file:', err);
-        }
-
-        // Parse the JSON data
-        let logEntries;
-        try {
-            logEntries = JSON.parse(data);
-            //console.log(logEntries)
-        } catch (err) {
-            console.error('Error parsing log file:', err);
-        }
-
-        // Process the log data
-        const contactDetails = [];
-
-        for (const entry of logEntries) {
-            const { NUMBER , DURATION, NAME } = entry;
-
-            if (NUMBER && DURATION) {
-                const existingContact = contactDetails.find((contact) => contact.NUMBER === NUMBER);
-
-                if (existingContact) {
-                    existingContact.callCount++;
-                    existingContact.DURATION += parseInt(DURATION);
-                } else {
-                    contactDetails.push({
-                        NUMBER,
-                        callCount: 1,
-                        DURATION : parseInt(DURATION),
-                        NAME : NAME
-                    });
-                }
-            }
-        }
-
-        if (arrangeBy) {
-            if (arrangeBy.toString().toLowerCase() === 'duration')
-                contactDetails.sort((a, b) => b.DURATION - a.DURATION);
-            else if (arrangeBy.toString().toLowerCase() === 'count')
-                contactDetails.sort((a, b) => b.callCount - a.callCount);
-        }
-
-
-        // Find the most contacted number and calculate total talk time
-        let mostContactedNumber = null;
-        let maxCount = 0;
-        let totalTalkTime = 0;
-
-        for (const contact of contactDetails) {
-            const { NUMBER, callCount, DURATION } = contact;
-
-            if (callCount > maxCount) {
-                mostContactedNumber = NUMBER;
-                maxCount = callCount;
-            }
-
-            totalTalkTime += parseInt(DURATION);
-        }
-
-        contactDetails.forEach((contact) => {
-            contact.DURATION = time.formatTime(contact.DURATION);
-        });
-
-        // Send the response
-        res.json({
-            mostContactedNumber,
-            contactDetails,
-            totalTalkTime : time.formatTime(totalTalkTime)
-        });
-
-    });
-
-}
-
-function processLogPhoneSpecific(res, phoneNo, logFilePath) {
-    console.log('started to analyze the log for the number'+ phoneNo+' ....')
-
-    // Read the log file
-    fs.readFile(logFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading log file:', err);
-        }
-
-        // Parse the JSON data
-        let logEntries;
-        try {
-            logEntries = JSON.parse(data);
-            //console.log(logEntries)
-        } catch (err) {
-            console.error('Error parsing log file:', err);
-        }
-
-        // Process the log data
-        const contactDetails = [];
-        let name = '';
-        let totalTalkTime = 0;
-        let OutgoingCount = 0;
-        let IncomingCount = 0;
-        let MissedCallCount = 0;
-
-
-        for (const entry of logEntries) {
-            const { NUMBER ,CALL_DATE, CALL_TYPE, DURATION, NAME } = entry;
-            if (NUMBER && DURATION && NUMBER.toString().replace(/(\+91|91)/g,'').trim() ===
-                phoneNo.replace(/(\+91|91)/g,'').trim() ) {
-                name = NAME;
-                totalTalkTime += parseInt(DURATION)
-                if (CALL_TYPE){
-                    if (CALL_TYPE.toString().toLowerCase() ==='incoming') IncomingCount++;
-                    else if (CALL_TYPE.toString().toLowerCase() === 'outgoing') OutgoingCount ++;
-                    else if (CALL_TYPE.toString().toLowerCase() === 'missed')MissedCallCount ++;
-                }
-                const existingContact = contactDetails.find((contact) => contact.CALL_DATE === time.convertDateStringToDDMMYYYY(CALL_DATE));
-
-                if (existingContact){
-                    existingContact.CALL_COUNT ++
-                    existingContact.DURATION += parseInt(DURATION)
-                }else contactDetails.push({
-                    CALL_DATE : time.convertDateStringToDDMMYYYY(CALL_DATE),
-                    CALL_COUNT : 1,
-                    DURATION : parseInt(DURATION)
-                })
-            }
-        }
-
-        contactDetails.forEach((contact) => {
-            contact.DURATION = time.formatTime(contact.DURATION);
-        });
-
-        // Send the response
-        res.json([{
-            QueryNumber : phoneNo,
-            NameSaved : name,
-            TotalTalkTime : time.formatTime(totalTalkTime),
-            OutgoingCount : OutgoingCount,
-            IncomingCount : IncomingCount,
-            MissedCount : MissedCallCount,
-            TotalRecordDays : contactDetails.length
-        },
-            contactDetails,
-        ]);
-
-    });
-}
-
 router.post('/logs/analyze', upload.single('logFile'), (req, res) => {
     // Access the uploaded file using req.file
     const file = req.file
     console.log(req.file)
     if (file){
         if (!file.originalname.toString().endsWith('.json')) res.json({error : 'not a valid supported log file type, try uploading a valid .json log file.'})
-        else processLog(res, req.query.arrangeBy, file.path)
+        else logAnalyser.processLog(res, req.query.arrangeBy, file.path)
     }else res.json({error : 'failed to get a valid log file'})
 });
 
@@ -192,7 +39,7 @@ router.post('/logs/analyze/queryPhone', upload.single('logFile'), (req, res) => 
     console.log(req.file)
     if (file){
         if (!file.originalname.toString().endsWith('.json')) res.json({error : 'not a valid supported log file type, try uploading a valid .json log file.'})
-        else processLogPhoneSpecific(res, req.query.phoneNo, file.path)
+        else logAnalyser.processLogPhoneSpecific(res, req.query.phoneNo, file.path)
     }else res.json({error : 'failed to get a valid log file'})
 });
 
